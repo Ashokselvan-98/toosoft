58b1564240511373a3a5251761028f92fc74dfa1
Ashokselvan-98 <161914313+Ashokselvan-98@users.noreply.github.com>
2024-03-07 23:32:22 +0530

    Update Ak.java

--- src/main/java/com/Onesoft/springdemo/Ak.java
+++ src/main/java/com/Onesoft/springdemo/Ak.java
@@ -7,15 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Ak {
 	@GetMapping("tk")
 	public String ak() {
-		return "She's beautiful, talented and intelligent as You can not ignore a Nisha, if you do, you have forgotten it till you die."
-				+ "Out of anyone any guy would be lucky to be with her or even have her as a friend."
-				+ "Diplomaitic and urbane."
-				+ "Keep him in your life when he comes across you. You might regret leaving him."
-				+ "Nisha is a very good listener and will give the best advices."
-				+ "Nisha thinks she's unattractive but every boy she walks past just stops and stares."+
-				" N----naughty"+" I----innocent"+" S----smiling face"+" H-----helping mind"+" A-----attractive"+"                            "
-						+ " ->->->->->->->->-->->->->->->->->->->->->->->->->->->->->->                                                                                              "
-				+" INTHA BIT INAIKU POTHUMA IYAASAMY"+"->->->->->->>->->->->->->->->->->->->->->->->->->->->-> ";
+		return "saturday leave podalamnu iruken enna solringa";
 	}
 
 }
